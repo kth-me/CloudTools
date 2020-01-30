3c48df6d749faf64e6d306ff13dce7fde01c2d0f
ujkkju117 <ujkkju117@outlook.com>
2020-01-31 00:24:08 +1030

    Sample code and removed the current reference from vsix project due to 3.1 SDK incompatibility for now. Tomorrow this will be looked into in implementing the API into the wrapper for the visual studio tool.

---
+++ CloudToolsUnitTesting/LinqMath.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="LinqMath.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CloudToolsUnitTesting
+{
+	using System.Collections.Generic;
+    using System.Linq;
+
+	/// <summary>
+	/// This is a Math class designed to fail at maths.
+	/// Since it's operation on numbers, it does not need
+	/// to be implemented but referenced as a static class.
+	/// </summary>
+	public static class LinqMath
+	{
+		/// <summary>
+		/// Function is declared static as it's part of a static class
+		/// Static classes can't have non-static methods.
+		/// </summary>
+		/// <param name="numbers">Array of numbers being parsed.</param>
+		/// <returns>incorrect value.</returns>
+		public static int Addition(IEnumerable<int> numbers)
+		{
+			var output = numbers.Sum();
+			return output;
+		}
+	}
+}
--- CloudToolsUnitTesting/UnitTest1.cs
+++ CloudToolsUnitTesting/UnitTest1.cs
@@ -1,11 +1,17 @@
-using NUnit.Framework;
+// <copyright file="UnitTest1.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace CloudToolsUnitTesting
 {
+    using System; // System based imports are always first
+    using System.Collections.Generic;
+    using NUnit.Framework; // Nuget based imports are second
+
     /// <summary>
     /// Generic test class to show how to document and run with pipeline.
     /// </summary>
-    public class Tests
+    public class UnitTest1
     {
         /// <summary>
         /// Set up process for unit testing.
@@ -16,12 +22,45 @@ namespace CloudToolsUnitTesting
         }
 
         /// <summary>
-        /// Test case 1 to be run
+        /// Test case 1 to be run.
         /// </summary>
         [Test]
         public void Test1()
         {
             Assert.Pass();
+        }
+
+        /// <summary>
+        /// Example case using LINQ to do our math.
+        /// </summary>
+        [Test]
+        public void LinqAddition()
+        {
+            // It is usually bad practice to generate random test data.
+            var r = new Random();
+            int result = 0;
+            List<int> randomNumbers = new List<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                var placeholder = r.Next(0, 100);
+                randomNumbers.Add(placeholder);
+                result += placeholder;
+            }
+
+            // Braces should be followed with a blank line.
+            // comments should also be followed by code
+            Console.WriteLine($"{LinqMath.Addition(randomNumbers)}:{result}");
+            Assert.IsTrue(LinqMath.Addition(randomNumbers) == result);
+
+            // Good practice would dictate testing known results e.g
+            int[] numbers = new int[2]{ 0, 1 };
+            Assert.IsTrue(LinqMath.Addition(numbers) == 1);
+            numbers = new int[3] { 0, 1, 2 };
+            Assert.IsTrue(LinqMath.Addition(numbers) == 3);
+
+            /* Tests should test for known Passing and known Failing conditions
+             * Due to using IEnumerator, The method may use arrays and lists
+             * And other itterable collection of int */
         }
     }
 }
