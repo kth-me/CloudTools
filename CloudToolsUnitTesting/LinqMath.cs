50c8a4f841df969f24b0b0efe16a9d8c306e4ccd
ujkkju117 <ujkkju117@outlook.com>
2020-02-02 18:32:06 +1030

    fixed tabbing and spacing.

--- CloudToolsUnitTesting/LinqMath.cs
+++ CloudToolsUnitTesting/LinqMath.cs
@@ -4,26 +4,26 @@
 
 namespace CloudToolsUnitTesting
 {
-	using System.Collections.Generic;
+    using System.Collections.Generic;
     using System.Linq;
 
-	/// <summary>
-	/// This is a Math class designed to fail at maths.
-	/// Since it's operation on numbers, it does not need
-	/// to be implemented but referenced as a static class.
-	/// </summary>
-	public static class LinqMath
-	{
-		/// <summary>
-		/// Function is declared static as it's part of a static class
-		/// Static classes can't have non-static methods.
-		/// </summary>
-		/// <param name="numbers">Array of numbers being parsed.</param>
-		/// <returns>incorrect value.</returns>
-		public static int Addition(IEnumerable<int> numbers)
-		{
-			var output = numbers.Sum();
-			return output;
-		}
-	}
+    /// <summary>
+    /// This is a Math class designed to fail at maths.
+    /// Since it's operation on numbers, it does not need
+    /// to be implemented but referenced as a static class.
+    /// </summary>
+    public static class LinqMath
+    {
+        /// <summary>
+        /// Function is declared static as it's part of a static class
+        /// Static classes can't have non-static methods.
+        /// </summary>
+        /// <param name="numbers">Array of numbers being parsed.</param>
+        /// <returns>incorrect value.</returns>
+        public static int Addition(IEnumerable<int> numbers)
+        {
+            var output = numbers.Sum();
+            return output;
+        }
+    }
 }
